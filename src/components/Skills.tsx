da117d2d7ef1f325a33133596541c552b2d4b33d
Trisonweru <isaactrisonwaweru@gmail.com>
2022-03-11 13:44:59 +0300

    fourth commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,14 @@ function Skills() {
   return (
     <div className='flex flex-wrap items-center justify-between px-3 space-y-4 w-full md:flex-nowrap md:space-x-8 md:space-y-0'>
       <div className='bg-[#112035] flex flex-col rounded-b-md shadow shadow-[#112035] w-full'>
-        <img src='/images/frontend.png' alt='frontend image' />
+        <div className='bg-white h-[300px]'>
+          <img
+            src='/images/Svg-3.svg'
+            alt='frontend image'
+            className='h-full object-fit rounded-md shadow-md w-full'
+          />
+        </div>
+
         <div className='px-3 py-3'>
           <h2 className='text-white text-xl'>Frontend Services</h2>
           <ul className='list-disc pt-3 px-8 text-sm text-white'>
@@ -16,7 +23,13 @@ function Skills() {
         </div>
       </div>
       <div className='bg-[#112035] flex flex-col rounded-b-md shadow shadow-[#112035] w-full'>
-        <img src='/images/backend.png' alt='frontend image' />
+        <div className='bg-white h-[300px]'>
+          <img
+            src='/images/Svg-4.svg'
+            alt='frontend image'
+            className='h-full object-fit rounded-md shadow-md w-full'
+          />
+        </div>
         <div className='px-3 py-3'>
           <h2 className='text-white text-xl'>Backend Services</h2>
           <ul className='list-disc pt-3 px-8 text-sm text-white'>
