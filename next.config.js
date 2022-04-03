8afa67c28501ae2e7714e46a154db3e0278defd3
Trisonweru <isaactrisonwaweru@gmail.com>
2022-04-03 17:39:05 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+// disable: process.env.NODE_ENV === 'development',
 /** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
-    register: true,
     disable: process.env.NODE_ENV === 'development',
-    skipWaiting: true,
-    runtimeCaching,
-    buildExcludes: [/middleware-manifest.json$/],
+    register: true,
+    scope: '/app',
+    sw: 'service-worker.js',
   },
   eslint: {
     dirs: ['src'],
