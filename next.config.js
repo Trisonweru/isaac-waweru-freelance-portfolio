651591da56df3e6b931ad50bf7fc12ae42e39d29
Trisonweru <isaactrisonwaweru@gmail.com>
2022-04-03 14:54:57 +0300

    pwa

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ const withPWA = require('next-pwa');
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    register: true,
     disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
   },
   eslint: {
     dirs: ['src'],
